0b86da49c6f06c58d748e9861e17a6df55d259b3
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-03-31 11:06:41 -0400

    data_processing changes

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,10 @@
 """Document that processes each text excerpt, and stores attributes"""
+from __future__ import annotations
 import csv
-from typing import Any, List, Optional, Tuple
+from typing import Optional
 
 
-def read_csv(csv_file: str) -> list[]:
+def read_csv(csv_file: str) -> list[TextBlock]:
     """Load network and packet data from a CSV file.
 
        Return a TODO of two values:
@@ -16,14 +17,29 @@ def read_csv(csv_file: str) -> list[]:
     with open(csv_file) as csv_fle:
         reader = csv.reader(csv_fle)
         headers = next(reader)
+        list_of_text_blocks = []
 
-        list_of_text_Blocks = []
         for row in reader:
+            # Turn the excerpt in each row into a list of Sentences
+            counter = 0
+            periods = [0]
+            # Find the indices of the end of the sentences
+            for x in row[7]:
+                if x == '.' or x == '?' or x == '!':
+                    periods.append(counter + 1)
+                counter += 1
+            # Create a Sentence for each sentence
+            sentences = []
+            for i in range(0, len(periods) - 1):
+                sentences.append(Sentence(row[7][periods[i]: periods[i + 1]], word_count=len(row[7].split(' '))))
+
             # initialize a text_block with the unique information of each text
+            TextBlock(id=int(row[0]), author=row[1], title=row[2], url=row[3], pub_year=int(row[4]),
+                      category=row[5], location=row[6], excerpt=sentences, carec_m=float(row[8]),
+                      sentence_count=counter)
+    # return a list of TextBlocks
+    return list_of_text_blocks
 
-            # assign each value to the Text Block Assignment
-
-    return list_of_text_Blocks
 
 def process_blocks(blocks_list: list[TextBlock]) -> list[tuple[int, list[Sentence]]]:
     """
@@ -32,7 +48,11 @@ def process_blocks(blocks_list: list[TextBlock]) -> list[tuple[int, list[Sentenc
 
     [id, [list of Sentences]
     """
-    # TODO: take each text block and create a list of sentences within it.
+    identifier = 0
+    new_lst = []
+    for i in blocks_list:
+        new_lst.append((identifier, i.excerpt))
+    return new_lst
 
 
 class TextBlock:
@@ -40,19 +60,51 @@ class TextBlock:
     Class storing the text for each novel entry, plus associated attributes from csv
 
     Instance Attributes:
-    TODO
-
+    - id: a unique identifier of a textblock
+    - author: the name of the person who wrote the excerpt
+    - title: the title of the book
+    - url:
+    - pub_year: the year of publication of the book
+    - category:
+    - location:
+    - excerpt: a portion of the book
+    - carec_m:
+    - sentence_count: the number of sentences in the excerpt
 
     """
-    sentences: list[str]
-    title: str
     id: int
-    category: str
+    author: str
+    title: str
+    url: str
     pub_year: int
+    category: str
+    location: str
+    excerpt: list[Sentence]
+    carec_m: float
     sentence_count: int
+
+    def __init__(self, id: int, author: str, title: str, url: str, pub_year: int, category: str, location: str,
+                 excerpt: list[Sentence], carec_m: float, sentence_count: int):
+        """initializes the instance attributes of TextBlock"""
+        self.id = id
+        self.author = author
+        self.title = title
+        self.url = url
+        self.pub_year = pub_year
+        self.category = category
+        self.location = location
+        self.excerpt = excerpt
+        self.carec_m = carec_m
+        self.sentence_count = sentence_count
 
     def average_sentence_length(self) -> float:
         """Returns the average number of words in a sentence"""
+        counter = 0
+        sum_so_far = 0
+        for i in self.excerpt:
+            sum_so_far += i.word_count
+            counter += 1
+        return sum_so_far / counter
 
 
 class Sentence:
@@ -60,22 +112,35 @@ class Sentence:
     Class to store each sentence and its associated attributes
 
     Instance Attributes:
-    TODO
+    - phrase: a sentence in str form
+    - word_count: the number of words in the phrase
 
+    Preconditions:
+    - self.phrase[-1] == '.' or self.phrase[-1] == '?' or self.phrase[-1] == '!'
+    - self.word_count > 0 or self.word_count is None
 
     """
     phrase: str
-    id: int
-    location: str
-    carec_m: float
+    # id: int
+    # location: str
+    # carec_m: float
     word_count: Optional[int]
 
-    def __init__(self):
+    def __init__(self, phrase: str, word_count: Optional[int] = None):
         """initializes the instance attributes of Sentence"""
+        self.phrase = phrase
+        # self.id = id
+        # self.location = location
+        # self.carec_m = carec_m
+        self.word_count = word_count
 
     def word_count(self):
         """returns number of words in sentence"""
-
+        return len(self.phrase.split(' '))
 
     def sentence_to_list(self):
         """Returns just the words of a sentence. """
+        temp = self.phrase.replace('.', '')
+        temp = temp.replace('!', '')
+        temp = temp.replace('?', '')
+        return temp
