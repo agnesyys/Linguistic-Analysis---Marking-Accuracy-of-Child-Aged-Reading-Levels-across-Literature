dfe410a19f5ccb3d3044a03badc0c0f0fafa110f
agnesyys <agnesyauys@gmail.com>
2023-04-02 13:25:30 -0400

    updated files

--- create_tree.py
+++ create_tree.py
@@ -1,15 +1,15 @@
-"""Creating a tree from a sentence using spacy"""
+"""Creating a tree from a sentence using spacy and nltk"""
 import nltk
 import spacy
 from nltk import Tree
+from typing import Any
 
 spacy.cli.download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
 
 
 def nltk_spacy_tree(sentence: str, attr_included: bool) -> nltk.tree:
-    """
-    Visualize the SpaCy dependency tree with nltk.tree
+    """Visualize the SpaCy dependency tree with nltk.tree
 
     if attr_included is True, then part of speech and syntatic dependency are included
     if attr_included is False, then only the words is included in the nltk.tree
@@ -21,8 +21,9 @@ def nltk_spacy_tree(sentence: str, attr_included: bool) -> nltk.tree:
     return tree[0]
 
 
-def token_format(token, attr_included) -> str:
-    """return its tokenized format, continuous string with all info
+def token_format(token, attr_included) -> Any:
+    """Return its tokenized format, continuous string with all info
+
     token.orth_: word (string representation)
     token.tag_: part of speech
     token.dep_: dependancy
@@ -33,8 +34,10 @@ def token_format(token, attr_included) -> str:
         return token.orth_
 
 
-def to_nltk_tree(node, attr_included) -> Tree | str:
-    """Return its tokenized format"""
+def to_nltk_tree(node, attr_included) -> Any:
+    """Return nltk tree of the sentence
+    If there's no subtrees, then return itself (the node)
+    """
     if node.n_lefts + node.n_rights > 0:
         return Tree(token_format(node, attr_included), [to_nltk_tree(child, attr_included) for child in node.children])
     else:
