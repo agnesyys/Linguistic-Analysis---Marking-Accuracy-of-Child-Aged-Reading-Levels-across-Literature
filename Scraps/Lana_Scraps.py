84bd67dcd259571ac271035cf1e769f35d6de076
Lana W <lanawehbeh12@gmail.com>
2023-04-02 16:58:53 -0400

    Helper for Agnes Plotly - Carec_m calculation

---
+++ Scraps/Lana_Scraps.py
@@ -0,0 +1,17 @@
+def get_most_similar(TextBlock: TeaxtBlock(Sentence("text sentence here"))):
+    dale_chall_complexity(text)
+    flesch_reading_ease(text)
+
+    poss_similarity_text_block = read_csv("    ")
+    min_diff = 100
+    for textblock in poss_similarity_text_block:
+        diff_dc = dc_score - textblock.dale_chall
+
+        diff_fe = dc_score - textblock.dale_chall
+
+        diff = diff_dc + diff_fe
+
+        if diff <= min_diff:
+            closest_text_block = textblock
+
+    return closest_text_block.carec_m
