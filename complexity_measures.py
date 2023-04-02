8d2fdae4f3da2940040c6608eb395e2100684524
Lana W <lanawehbeh12@gmail.com>
2023-04-02 10:25:29 -0400

    Mostly Complete Complexity Measures -> Need to fix one error in syntax tree and implement standardized scoring

--- complexity_measures.py
+++ complexity_measures.py
@@ -1,7 +1,18 @@
 """Given a syntax tree, these are implementations of the different collections of complexity measures"""
-from data_processing import TextBlock
+from data_processing import TextBlock, Sentence
+import create_tree as ct
 import csv
 
+import nltk
+# Import required libraries
+import spacy
+from nltk import Tree
+from spacy import displacy
+from pathlib import Path
+
+# spacy.cli.download("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
+from typing import Any, Optional
 
 def dale_chall_complexity(text: TextBlock) -> float:
     """
@@ -124,3 +135,171 @@ def num_syllables(word: str) -> int:
         i += 1
 
     return syll_num
+
+# -----------------------------------------
+def mean_dependency_distance(sentence: str):
+    """Calculates the mean_dependency_distance given a sentence
+
+    The Mean Dependency Distance (MDD) is:
+    MDD(the sentence) = 1/(n-1) * summation of |D*Di| from i = 1 to n-1
+
+    where DDi is the DD (dependency distance) of the ith syntactic link in the sentence.
+    Note that the root of the sentence does not have a governor, and so will have a DD of 0.
+
+    The dependency distance of adjacent words is 1, and dependency distance is defined
+    as the (absolute) distance between the governor word and its dependent.
+
+    Example:
+        "The girl ate an apple" has dependencies:
+        The: 1 (dependent of girl, dist 1)
+        girl: 1 (dependent of ate, dist 1)
+        ate: 0 (verb, has no governer, not dependent of anything)
+        an: 1 (dependent of apple, dist 1)
+        apple: 2 (dependent of ate, distance 2)
+    """
+    # Generally: this function calculates the distance between each word and its dependent in the sentence,
+    # by traversing through the tree.
+    # to calculate MDD, we begin with creating a dependency tree.
+    tree = nltk_spac_tree(sentence, False)
+    # tree.pretty_print()
+
+    # Then, for each word in the tree, which we refer to as the ith word based on sentence position
+    # we need to get, for every subtree, the distance between root and children if there is only one child.
+
+    dependents = []
+    flatten(get_dependents(tree), dependents)
+    # we can now take every pair to get their distances, by finding their position in the sentence.
+    # Since dependency tree is formed left to right, going from i =0  to i = len(sentence) - 1
+    # will maintain the order in case of duplicates
+    distances = []
+    for i in range(0, len(sentence), 2):
+        distances.append(abs(sentence.index(dependents[i], i - 1) - sentence.index(dependents[i + 1], i - 1)))
+
+    # lastly, summ all DDs and divide by num of words in sentence
+    return sum(distances) / len(sentence)
+
+
+def get_dependents(tree: nltk.tree) -> list[list[nltk.tree]] | None:
+    """Get dependents in pairs
+
+    Note that the NLTK built-in methods for nltk.tree in this function add the following conditions:
+    tree.subtrees() returns ALL the constituent trees, this includes the subtrees of its subtrees (and so on).
+    It does not return ANY leaves, even those which are direct children of the tree's root.
+    It also returns itself as one of the listed subtrees.
+
+    tree.leaves() returns ALL the leaves across the full tree, i.e all the descendants which are leaves
+
+    tree.label() is analogous to the in-class tree.root, and returns the root value of the tree
+    """
+    # Then, for each word in the tree, which we refer to as the ith word based on sentence position,
+    tree_root = tree.label()
+    dependents = []
+
+    # we need to get, for every subtree, the distance between root and children if there is a direct child leaf.
+    if len(tree.leaves()) >= 1:
+        # all leaves, direct subs are not included
+        for leaf in tree.leaves():
+            subtrees_sans_original = [subtree for subtree in tree.subtrees() if subtree.label() != tree.label()]
+            direct_leaf_condition = not any(leaf in subtree.leaves() for subtree in subtrees_sans_original)
+            if direct_leaf_condition:
+                dependents.append([tree_root, leaf])
+
+    for subtree in tree.subtrees():
+        if subtree.label() != tree_root:
+            # create a pair with root and every child, confirm that the subtree is a direct child of tree
+            subtree_direct_child_of_tree_root = []
+            if subtree_direct_child_of_tree_root:
+                dependents.append([subtree.label(), tree_root])
+            print([subtree.label(), tree_root])
+            dependents.append(get_dependents(subtree))
+
+    return dependents
+
+def is_empty(tree) -> bool:
+    """Return whether this tree is empty.
+    """
+    return tree.label() is None
+
+def len_tree(tree: nltk.tree):
+    if tree.leaves() == []:
+        return 0
+    else:
+        size = 1  # count the root
+        for subtree in tree.subtrees():
+            size += len_tree(subtree)  # could also write len(subtree)
+        return size
+
+def flatten(nested_list: str | list, unnested: list) -> None:
+    """Return the items of the given nested list.
+
+    This version uses a comprehension and the built-in sum aggregation function.
+    """
+    if isinstance(nested_list, str):
+        unnested.append(nested_list)
+    else:
+        unnested.append(flatten(sublist, []) for sublist in nested_list)
+
+
+def nltk_spac_tree(sentence: str, attr_included: Optional[bool] = False) -> nltk.tree:
+    """
+    Visualize the SpaCy dependency tree with nltk.tree
+    """
+    # gets all the tokenized info
+    doc = nlp(sentence)
+    tree = [to_nltk_tree(sentence.root, attr_included) for sentence in doc.sents]
+    # The first item in the list is the full tree
+    return tree[0]
+    # tree[0].draw()
+
+def to_nltk_tree(node, attri_included):
+    """Return its tokenized format"""
+    if node.n_lefts + node.n_rights > 0:
+        return Tree(token_format(node, attri_included), [to_nltk_tree(child, attri_included) for child in node.children])
+    else:
+        return token_format(node, attri_included)
+
+def token_format(token, attrib_included):
+    """return its tokenized format as a continuous string with all info
+    token.orth_: word (string representation)
+    token.tag_: part of speech
+    token.dep_: dependancy
+    """
+    if attrib_included:
+        return "_".join([token.orth_, token.tag_, token.dep_])
+    else:
+        return token.orth_
+
+
+
+# TODO: standardize complexity measures scale
+def standardized_dale_chall(dc_score: float) -> float:
+    """Standardizes DC score using the following metric:
+
+
+
+
+
+    """
+    return dc_score
+
+
+def standardized_flesch_kincaid(fk_score: float) -> float:
+    """Standardizes FK score using the following metric:
+
+
+
+
+
+    """
+    return fk_score
+
+
+def standardized_syntax_score(syn_score: float) -> float:
+    """Standardizes FK score using the following metric:
+
+
+
+
+
+    """
+    return syn_score
