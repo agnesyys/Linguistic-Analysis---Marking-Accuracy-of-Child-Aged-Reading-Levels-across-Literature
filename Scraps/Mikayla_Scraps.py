e5240d652239689c0364f70556828e8aa1d7c8cc
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-04-03 09:38:04 -0400

    main changes

--- Scraps/Mikayla_Scraps.py
+++ Scraps/Mikayla_Scraps.py
@@ -1,7 +1,9 @@
 """Storage for Mikayla in case of Git Shenanigans"""
 # import plotly.graph_objects as go
 import nltk
-# from courseProject.data_processing import TextBlock, Sentence
+from courseProject.data_processing import read_csv
+from courseProject.data_processing import TextBlock, Sentence
+from courseProject.complexity_measures import mean_dependency_distance
 # from courseProject import complexity_measures as complex
 
 
@@ -26,39 +28,28 @@ import nltk
 #     )
 #     fig.show()
 
+def runner() -> None:
+    """A runner of the data_set_novels.csv file."""
+    textblocks = read_csv('data/data_set_novels.csv')
+    counter = 0
+    dc = []
+    fc = []
+    dependency = []
+    CAREC = []
+    for textblock in textblocks:
+        dc.append(textblock.dale_chall)
+        fc = textblock.flesch_reading
+        dependency.append(mean_dependency_distance(textblock, False))
+        CAREC.append(textblock.carec_m)
+        counter += 1
+    avg_dc = sum(dc) / counter
+    avg_fc = sum(fc) / counter
+    avg_dependency = sum(dependency) / counter
+    avg_CAREC = sum(CAREC) / counter
 
-def get_dependents(tree: nltk.tree) -> list[list[nltk.tree]] | None:
-    """Get dependents in pairs.
-
-    Note: that the NLTK built-in methods for nltk.tree in this function add the following conditions:
-    tree.subtrees() returns ALL the constituent trees, this includes the subtrees of its subtrees (and so on).
-    It does not return ANY leaves, even those which are direct children of the tree's root.
-    It also returns itself as one of the listed subtrees.
-
-    tree.leaves() returns ALL the leaves across the full tree, i.e. all the descendants which are leaves
-
-    tree.label() is analogous to the in-class tree.root, and returns the root value of the tree
-    """
-    # Then, for each word in the tree, which we refer to as the i-th word based on sentence position,
-    tree_root = tree.label()
-    dependents = []
-
-    # for subtree in tree.subtrees():
-    #     tree.
-
-    # we need to get, for every subtree, the distance between root and children if there is a direct child leaf.
-    # if len(tree.leaves()) >= 1:
-    #     for leaf in tree.leaves():
-    #         subtrees_sans_original = [subtree for subtree in tree.subtrees() if subtree.label() != tree_root]
-    #         direct_leaf_condition = not any(leaf in subtree.leaves() for subtree in subtrees_sans_original)
-    #         if direct_leaf_condition:
-    #             dependents.append([tree_root, leaf])
-    # print(dependents)
-    # # Iterate through all subtrees (note that leaves are not included here), and if the subtree is not itself,
-    # # store the subtree's root and this tree's root in list of dependents
-    # # Recurse into the subtree to collect all the parent-child pairs inside
-    # for subtree in tree.subtrees:
-    #     if subtree.label() != tree_root:
-    #         dependents.append([tree_root, subtree.label()])
-
-    return dependents
+    fig = go.Figure(
+            data=[go.Bar(y=[avg_dc, fc, sd, textblock.carec_m], x=['Dale-Chall Complexity', 'Flesch Complexity',
+                                                              'Syntatic Difficulty', 'CAREC_M'])],
+            layout_title_text="Reading Levels Accuracy Compared to CAREC M of '" + textblock.title + "'"
+        )
+        fig.show()
